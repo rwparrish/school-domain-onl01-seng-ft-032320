d61ddf8e486c171dfcc898e5c9fa45867ad7d3f8
Ryan Parrish <rino3717@gmail.com>
2020-04-03 01:16:19 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -9,22 +9,22 @@ class School
   
   
   def add_student(student, grade)
-    if roster.has_key?(grade) 
-      roster[grade] << student
+    if  @roster.has_key?(grade) 
+      @roster[grade] << student
     else 
-      roster[grade] = []
-      roster[grade] << student
+      @roster[grade] = []
+      @roster[grade] << student
     end
   end
   
   def grade(grade)
-    roster[grade]
+      @roster[grade]
   end
   
   def sort
-      roster.collect do |grade,name|
-      roster[grade] = name.sort
-    end
-    roster
+     @roster.map do |grade, name|
+     @roster[grade] = name.sort
+  end
+    @roster
   end
 end
